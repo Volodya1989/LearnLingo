9142be4adffa883f172867daf05f3650258467ab
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2024-01-08 17:08:08 -0500

    shared layout added

---
+++ src/components/MainHome/MainHome.jsx
@@ -0,0 +1,14 @@
+const MainHome = () => {
+  return (
+    <div>
+      <div></div>
+      <p>Unlock your potential with the best language tutors</p>
+      <p>
+        Embark on an Exciting Language Journey with Expert Language Tutors:
+        Elevate your language proficiency to new heights by connecting with
+        highly qualified and experienced tutors.
+      </p>
+    </div>
+  );
+};
+export default MainHome;
