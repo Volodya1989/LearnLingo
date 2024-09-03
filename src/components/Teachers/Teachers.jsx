46af275976cfd49030c0f5e1192ae08711a6258a
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2024-09-03 07:06:08 -0400

    adding dropdonw filter -1

--- src/components/Teachers/Teachers.jsx
+++ src/components/Teachers/Teachers.jsx
@@ -4,12 +4,24 @@ import teachersAPI from '../../data/teachers.json';
 import useLocalStorage from 'hooks/useLocalStorage';
 import useFetch from 'use-http';
 import { StyledContainer } from './Teachers.styled';
-
+// import Search from 'components/Search';
 const Teachers = () => {
   // console.log(teachersAPI);
   const [teachers, setTeachers] = useLocalStorage('teachers', null);
   const { loading } = useFetch();
   const [isLoading, setIsLoading] = useState(false);
+  // const [query, setQuerySearch] = useState('');
+
+  // //setting query state on change and passing it as props to search component
+  // const onQueryChange = useCallback(
+  //   e => {
+  //     if (e.currentTarget.value === ' ') {
+  //       return;
+  //     }
+  //     setQuerySearch(e.currentTarget.value);
+  //   },
+  //   [setQuerySearch]
+  // );
 
   const handleLoading = useCallback(
     e => {
@@ -34,7 +46,14 @@ const Teachers = () => {
 
   return (
     <StyledContainer>
-      {!isLoading ? <p>Loading...</p> : <CardsList teachers={teachers} />}
+      {!isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {/* <Search onQueryChange={onQueryChange} query={query} /> */}
+          <CardsList teachers={teachers} />
+        </>
+      )}
     </StyledContainer>
   );
 };
