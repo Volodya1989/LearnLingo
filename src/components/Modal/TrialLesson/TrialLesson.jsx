43ee2eb54d72962f29b5cf2f75fecf464808cb4a
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2024-09-13 20:59:58 -0400

    react-hook-form added

--- src/components/Modal/TrialLesson/TrialLesson.jsx
+++ src/components/Modal/TrialLesson/TrialLesson.jsx
@@ -1,5 +1,63 @@
-import { Description, Text, Heading } from './TrialLesson.styled';
+import { useForm } from 'react-hook-form';
+import { useCallback, useEffect } from 'react';
+import useLocalStorage from 'hooks/useLocalStorage';
+
+import {
+  Description,
+  Text,
+  Heading,
+  Label,
+  Field,
+  Wrapper,
+} from './TrialLesson.styled';
 export const TrialLesson = ({ details: { surname, name } }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    formState: { isSubmitSuccessful },
+  } = useForm();
+  const [fullName, setFullName] = useLocalStorage('fullName' || '');
+  const [email, setEmail] = useLocalStorage('email' || '');
+  const [phone, setPhone] = useLocalStorage('phone' || '');
+
+  //setting query state on change and passing it as props to search component
+  const onQueryChange = useCallback(
+    e => {
+      console.log(e.currentTarget.name);
+      if (e.currentTarget.value === ' ') {
+        return;
+      }
+      if (e.currentTarget.name === 'email') {
+        setEmail(e.currentTarget.value);
+      }
+      if (e.currentTarget.name === 'fullName') {
+        setFullName(e.currentTarget.value);
+      }
+      if (e.currentTarget.name === 'phone') {
+        setPhone(e.currentTarget.value);
+      }
+      // setQuerySearch(e.currentTarget.value);
+    },
+    [setEmail, setFullName, setPhone]
+  );
+  const onClick = data => {
+    setFullName('');
+    setPhone('');
+    setEmail('');
+    console.log(data);
+  };
+
+  useEffect(() => {
+    console.log(isSubmitSuccessful);
+
+    if (isSubmitSuccessful) {
+      console.log('deleting');
+      setFullName('');
+      setPhone('');
+      setEmail('');
+    }
+  }, [setEmail, setFullName, setPhone, isSubmitSuccessful]);
   // const { showBridge, description, brand, model } = details;
   return (
     <>
@@ -22,6 +80,45 @@ export const TrialLesson = ({ details: { surname, name } }) => {
           Our experienced tutor will assess your current language level, discuss
           your learning goals, and tailor the lesson to your specific needs.
         </Text>
+        <form onSubmit={handleSubmit(data => onClick(data))}>
+          {errors.fullName && <p>Full Name is required.</p>}
+          <Wrapper>
+            <Field
+              {...register('fullName', { required: true, value: fullName })}
+              onChange={onQueryChange}
+              name="fullName"
+              value={fullName}
+              autoComplete="off"
+              type={'text'}
+            />
+            <Label htmlFor={1}>{'Full Name'}</Label>
+          </Wrapper>
+          <Wrapper>
+            <Field
+              {...register('email', { required: true, value: email })}
+              onChange={onQueryChange}
+              name="email"
+              value={email}
+              autoComplete="off"
+              type={'text'}
+            />
+            <Label htmlFor={1}>{'Email'}</Label>
+          </Wrapper>
+          {errors.email && <p>Email is required.</p>}
+          <Wrapper>
+            <Field
+              {...register('phone', { required: true, value: phone })}
+              onChange={onQueryChange}
+              name="phone"
+              value={phone}
+              autoComplete="off"
+              type={'text'}
+            />
+            <Label htmlFor={1}>{'Phone'}</Label>
+            {errors.phone && <p>Phone number is required.</p>}
+          </Wrapper>
+          <input type="submit" />
+        </form>
       </Description>
     </>
   );
--- src/components/Modal/TrialLesson/TrialLesson.styled.jsx
+++ src/components/Modal/TrialLesson/TrialLesson.styled.jsx
@@ -36,3 +36,65 @@ export const Heading = styled.h5`
   margin-bottom: 25px;
   text-align: center;
 `;
+export const Label = styled.label`
+  font-size: 22px;
+  font-weight: 300;
+  width: 200px;
+  color: black;
+  position: absolute;
+  left: 0;
+  top: 30%;
+  transform: translateY(-50%);
+  transition: all 0.2s ease;
+  pointer-events: none;
+`;
+
+export const Wrapper = styled.section`
+  flex-direction: 'row';
+  alignitems: 'center';
+  position: relative;
+  margin-bottom: 40px;
+`;
+
+export const Field = styled.input`
+  display: flex;
+  border: 1px solid gray;
+  font-size: 22px;
+  background-color: transparent;
+  padding: 10px 15px;
+  border-bottom: 2px solid #d5f5f4;
+  outline: none;
+  width: 200px;
+  color: black;
+  @media screen and (min-width: 768px) {
+    width: 350px;
+  }
+  // @media screen and (min-width: 1100px) {
+  //   width: 450px;
+  // }
+
+  ${({ disabled }) =>
+    (disabled || '') &&
+    `
+    pointer-events: none;
+  `}
+
+  &:focus {
+    border-color: #6118de;
+  }
+
+  &:focus ~ ${Label} {
+    transform: translateY(-55px);
+    scale: 0.9;
+    color: #9ca5b5;
+  }
+
+  ${({ value = '' }) =>
+    value &&
+    `
+    & ~ ${Label} {
+      transform: translateY(-50px);
+
+    }
+  `}
+`;
